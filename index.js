ad29240d72cc7ee640b21043e175777797b58524
Ziqiang Li <ziqiang.lee@gmail.com>
2017-08-31 15:44:19 +0800

    add cors worker

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lizq on 2017/8/31
+ */
+function CORSWorker( url ) {
+    let promise = new Promise( ( resolve, reject ) => {
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener( 'load', () => {
+            try {
+                let blob   = window.URL.createObjectURL( new Blob( [ this.responseText ] ) );
+                let worker = new Worker( blob );
+                resolve( worker );
+            } catch ( e ) {
+                reject( e );
+            }
+        }, false );
+        xhr.addEventListener( 'error', ( err ) => {
+            reject( err );
+        }, false );
+        xhr.open( 'GET', url, true );
+        xhr.send();
+    } );
+    return promise;
+}
+
+export default CORSWorker;
